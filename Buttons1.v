341dbfae1eecbaaaf2b79e9cf06d3620f44893da
fpgacourse <steveatpsu@yahoo.com>
2015-09-24 20:35:09 -0700

    Verilog code for tieing on-board Elbert buttons to LEDs

---
+++ Buttons1.v
@@ -0,0 +1,18 @@
+module buttons1(
+    input but,
+    input but1,
+    input butext,
+    input butext1,
+    output led,
+    output led1,
+    output ledext,
+    output ledext1
+    );
+	 
+	 assign led =    ~but;
+	 assign led1 =   ~but1;
+	 assign ledext = ~butext;
+	 assign ledext1 = ~butext1;
+
+
+endmodule
